5e09db0b17c36e60b19f277941eb683495451b34
aysha-12 <munniaysha555@gmail.com>
2024-11-04 17:29:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,163 @@
+// // Q:1
+ var barger=500;
+ var elesCoke=30;
+
+ var cost= barger + elesCoke;
+
+ console.log ("cost=",cost);
+
+ // Q:2
+
+var height=31*31;
+var weight=50;
+
+var BMI = height/ weight;
+console.log(31*31/50)
+var BMI=19.22;
+
+
+if(BMI==19.22){
+    if(BMI<18.5){
+        console.log('underweight')
+    }
+    else(
+        console.log('healthy')
+    )
+}
+if(BMI==19.22){
+    if(BMI>=18.5 && BMI<=24.9){
+        console.log('normal')
+    }
+    else{
+        console.log('not good')
+    }
+}
+if(BMI==19.22){
+    if(BMI>=25 && BMI<=29.9){
+        console.log('overweight')
+    }
+    else{
+        console.log('normal')
+    }
+}
+
+
+// // Q:3
+
+ var score=70;
+ var grade;
+
+ if(score>=90 && score<=100){
+    grade="A";
+    console.log (grade);
+ }
+ else if (score>=80 && score<=89){
+     grade="B"
+ }
+ else if (score>=70 && score<=79){
+     grade="C"
+ }
+ else if (score>=60 && score<=69){
+     grade="D"
+ }
+ else if (score>=0 && score<=59){
+     grade="F"
+ }
+
+ console.log (grade);
+
+
+// Q:4
+
+var score=80;
+var someone=65;
+var friend=80;
+var value=38;
+var num=70;
+
+if(score==80){
+    if(friend>=80){
+        console.log("we will go for lunch")
+    }
+    else{
+        console.log ("we will not go")
+    }
+}
+if(score==80){
+    if(someone<80 && someone>=60){
+        console.log("good luck")
+
+    }
+    else{
+        console.log("bad luck")
+    }
+}
+if(score==80){
+    if( value>=40 && value<=60){
+        console.log("massage unseen")
+    }
+    else{
+        console.log("massage seen")
+    }
+}
+if( score == 80){
+    if( value>40){
+        console.log("block him")
+    }
+    else{
+        console.log("ok")
+    }
+}
+if(score=80){
+    if(num<80){
+        console.log("sad")
+    }
+    else{
+        console.log("happy")
+    }
+
+}
+
+// Q:5
+
+var num1=40;
+var num2=20;
+
+var result=num1+num2;
+console.log(num1+num2)
+
+
+
+// Q:6
+
+
+var tickfare=800;
+
+
+var total=3000;
+
+var student=400;
+console.log(800*50/100)
+
+var senior=680;
+console.log(800*15/100-tickfare)
+
+var cost=student+senior;
+console.log(cost)
+
+var backAmount=total-cost;
+console.log(backAmount)
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
